7c2eeeb0aa24f562013d3b81014fe9a0f332a83d
Tiago Cardoso <tiago.mncardoso@gmail.com>
2019-11-22 17:36:34 -0300

    Alterando primeiro exemplo

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,24 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Frases de Santo Agostinho'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,67 +27,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _numeroAleatorio = 0;
+  List _frases = [
+    'O orgulho é a fonte de todas as fraquezas, por que é a fonte de todos os vícios.',
+    'Milagres não são contrários à natureza, mas apenas contrários ao que nós sabemos sobre a natureza.',
+    'Se não podes entender, crê para que entendas. A fé precede, o intelecto segue.',
+    'Foi o orgulho que transformou anjos em demônios, mas é a humildade que faz de homens anjos.',
+    'O mundo é um livro, e quem fica sentado em casa lê somente uma página.',
+    'Creio para compreender, e compreendo para crer melhor.',
+    'Ser cristão não é conquistar Cristo, mas deixar-se conquistar por Ele. Deixa que Ele conquiste em ti, que Ele conquiste para ti, que Ele te conquiste.',
+    'Na essência somos iguais, nas diferenças nos respeitamos.',
+    'No amor do próximo o pobre é rico; sem amor do próximo o rico é pobre.',
+    'Se o homem soubesse as vantagens de ser bom, seria homem de bem por egoísmo.',
+    'Há homens que se agarram a sua opinião, não por ser verdadeira, mas simplesmente por ser sua.'
+  ];
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _numeroAleatorio = new Random().nextInt(10);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Santo Agostinho disse:',
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              _frases[_numeroAleatorio],
+              style: Theme.of(context).textTheme.caption,
+              textAlign: TextAlign.center
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        tooltip: 'Gera nova frase',
+        child: Icon(Icons.cached),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
